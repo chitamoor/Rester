d00bb530db96903b574a429e498a9c80503ead7e
Rajeev Chitamoor <rajeev@chitamoor.com>
2014-04-07 23:02:22 -0700

    Added support for test suites

--- apirunner.py
+++ apirunner.py
@@ -2,22 +2,34 @@ import logging
 import argparse
 from testcase import ApiTestCaseRunner
 
+DEFAULT_TEST_CASE = 'test_case.json'
+
 def parse_cmdln_args():
     parser = argparse.ArgumentParser(description='Process command line args')
     parser.add_argument('--log', help='log help', default='INFO')
     parser.add_argument(
-        '--tc', help='test_case help', default='test_case.json')
+        '--tc', help='tc help')
+    parser.add_argument(
+        '--ts', help='ts help')
 
     args = parser.parse_args()
-    return (args.log.upper(), args.tc)
+    return (args.log.upper(), args.tc, args.ts)
 
 if (__name__ == '__main__'):
-    log_level, test_case = parse_cmdln_args()
-    print log_level, test_case
+    log_level, test_case, test_suite = parse_cmdln_args()
+    print log_level, test_case, test_suite
     logging.basicConfig()
     logger = logging.getLogger('ApiTestCaseRunner')
     logger.setLevel(log_level)
     print log_level
     test_runner = ApiTestCaseRunner(logger)
-    test_runner.run_test_case(test_case)
+    if test_case is not None:
+        print "test case has been specified"
+        #test_runner.run_test_case(test_case)
+    elif test_suite is not None:
+        print "test suite has been specified"
+        test_runner.run_test_suite(test_suite)
+    else:
+        print "running the default test case"
+        #test_runner.run_test_case(DEFAULT_TEST_CASE)
     test_runner.display_report()
